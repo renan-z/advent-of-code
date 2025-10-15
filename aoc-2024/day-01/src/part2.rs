5cb742e91cbe0b4dee13fcc3ac2d1051d9ff5459
An Nguyen <an.nguyen.yn@renesas.com>
2025-10-15 08:33:04 +0000

    Solved D1P2

--- aoc-2024/day-01/src/part2.rs
+++ aoc-2024/day-01/src/part2.rs
@@ -1,6 +1,28 @@
+use regex::Regex;
+
+pub fn parser(input: &str) -> String {
+    let re = Regex::new(r"(\d+)\s+(\d+)").unwrap();
+    let mut sum = 0;
+    let mut first_list: Vec<i32> = Vec::new();
+    let mut second_list: Vec<i32> = Vec::new();
+    for line in input.lines() {
+        let caps = re.captures(line).unwrap();
+        let a = caps.get(1).unwrap().as_str().parse::<i32>().unwrap();
+        let b = caps.get(2).unwrap().as_str().parse::<i32>().unwrap();
+        // Add first number to first list
+        // Add second number to second list
+        first_list.push(a);
+        second_list.push(b);
+    }
+    // Adding up each number in the first list after multiplying it by the number of times that number appears in the second list.
+    for i in first_list {
+        sum += i * second_list.iter().filter(|&&x| x == i).count() as i32;
+    }
+    sum.to_string()
+}
+
 pub fn process(input: &str) -> String {
-    println!("Input 2: {}", input);
-    "Hello".to_string()
+    parser(input)
 }
 #[cfg(test)]
 mod tests {
@@ -8,7 +30,12 @@ mod tests {
 
     #[test]
     fn test_process() {
-        let input = "Hello";
-        assert_eq!("Hello", process(input))
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+        assert_eq!("31", process(input))
     }
 }
