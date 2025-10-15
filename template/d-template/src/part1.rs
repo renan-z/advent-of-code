ac20a9088f6707a2df4e98df42d78d959e4303d3
An Nguyen <an.nguyen.yn@renesas.com>
2025-10-15 06:31:15 +0000

    First version of template

---
+++ template/d-template/src/part1.rs
@@ -0,0 +1,13 @@
+pub fn process(input: &str) -> String {
+    "Hello".to_string()
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() {
+        let input = "Hello";
+        assert_eq!("Hello", process(input))
+    }
+}
